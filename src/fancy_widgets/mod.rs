abfd134a68fcf06f3873d380e98ed672cea3ad94
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2023-08-14 08:12:23 +0200

    Document fancy_widgets

--- src/fancy_widgets/mod.rs
+++ src/fancy_widgets/mod.rs
@@ -1,2 +1,7 @@
+///! This module contains custom widgets for iced that are either
+///! faster, more customizable, or with more features.
+///!
+///! I also contains module with svg icons.
+
 pub mod icons;
 pub mod wrap_box;
