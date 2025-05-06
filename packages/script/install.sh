da8945e002f74aa8c37a46c9eae45501b1292eb1
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2025-05-06 17:36:57 +0200

    Better gzip compression.

--- packages/script/install.sh
+++ packages/script/install.sh
@@ -203,8 +203,8 @@ $_CARGO build -r
 
 echo "Building man pages..."
 mkdir -p target/manpages
-"$_GZIP" --fast other/manpages/uamp.1 > target/manpages/uamp.1.gz
-"$_GZIP" --fast other/manpages/uamp.5 > target/manpages/uamp.5.gz
+"$_GZIP" --best other/manpages/uamp.1 > target/manpages/uamp.1.gz
+"$_GZIP" --best other/manpages/uamp.5 > target/manpages/uamp.5.gz
 
 echo "Moving files to system..."
 echo "Sudo is required for this final step."
