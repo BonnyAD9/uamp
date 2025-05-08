25506c879b990716213ff56f7d8ced475464843a
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2025-05-08 13:58:57 +0200

    Fmt.

--- src/env/update/mode.rs
+++ src/env/update/mode.rs
@@ -1,4 +1,4 @@
-use pareg::{starts_any, val_arg, ArgErrCtx, ArgError, FromArg};
+use pareg::{ArgErrCtx, ArgError, FromArg, starts_any, val_arg};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
@@ -13,11 +13,18 @@ impl<'a> FromArg<'a> for Mode {
     fn from_arg(arg: &'a str) -> pareg::Result<Self> {
         match arg {
             "tag" | "latest-tag" | "LatestTag" => Ok(Self::LatestTag),
-            "commit" | "latest-commit" | "LatestCommit" => Ok(Self::LatestCommit),
-            v if starts_any!(v, "branch=", "Branch=") => Ok(Self::Branch(val_arg(arg, '=')?)),
-            _ => ArgError::InvalidValue(ArgErrCtx::from_msg("Invalid update mode.", arg.to_string()).into())
-                .hint("Valid options are: `tag`, `commit`, `branch=<branch>`.")
-                .err()
+            "commit" | "latest-commit" | "LatestCommit" => {
+                Ok(Self::LatestCommit)
+            }
+            v if starts_any!(v, "branch=", "Branch=") => {
+                Ok(Self::Branch(val_arg(arg, '=')?))
+            }
+            _ => ArgError::InvalidValue(
+                ArgErrCtx::from_msg("Invalid update mode.", arg.to_string())
+                    .into(),
+            )
+            .hint("Valid options are: `tag`, `commit`, `branch=<branch>`.")
+            .err(),
         }
     }
 }
