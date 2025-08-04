95c4902a51cb5c8d4362ceaa77e64490f6a980e7
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2025-08-04 13:41:13 +0200

    Fix newlines in sse.

--- src/core/server/sub_msg.rs
+++ src/core/server/sub_msg.rs
@@ -9,7 +9,7 @@ impl SubMsg {
     pub fn event(&self) -> Result<String> {
         match self {
             Self::SetAll(a) => Ok(format!(
-                "event: set-all\ndata: {}",
+                "event: set-all\ndata: {}\n\n",
                 serde_json::ser::to_string(a)?
             )),
         }
