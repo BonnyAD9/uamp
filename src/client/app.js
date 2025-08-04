8724e777b34857c774781b163e6e2676aa7e6fd5
Martan03 <martinslezak03@gmail.com>
2025-08-04 14:07:50 +0200

    Add basic settings

--- src/client/app.js
+++ src/client/app.js
@@ -1,5 +1,23 @@
 const API_URL = 'http://localhost:8267/api';
 
+const navs = document.querySelectorAll('nav p');
+const screens = document.querySelectorAll('.screen');
+
+navs.forEach(item => {
+    item.addEventListener('click', () => {
+        navs.forEach(p => p.classList.remove('active'));
+        item.classList.add('active');
+
+        const targetId = item.dataset.screen;
+        for (let screen of screens) {
+            screen.classList.remove('active');
+            if (screen.id == targetId) {
+                screen.classList.add('active');
+            }
+        }
+    });
+});
+
 function apiCtrl(query) {
     fetch(`${API_URL}/ctrl?${query}`)
         .then(res => {
