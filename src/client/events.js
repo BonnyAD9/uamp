b8eea1d32857ef07cd95cabb680b600e94cd4bed
Martan03 <martinslezak03@gmail.com>
2025-08-05 08:30:50 +0200

    Add more SSE event handling and song progress bar

--- src/client/events.js
+++ src/client/events.js
@@ -21,27 +21,36 @@ eventSource.addEventListener('set-all', e => {
     app.updateAll();
 });
 
-eventSource.addEventListener('set-playlist', e => { });
+eventSource.addEventListener('set-playlist', e => {
+    const app = AppSingleton.get();
+    const data = JSON.parse(e.data);
+
+    setPlayback(app, data.playback);
+    app.player.playlist = data.playlist;
+    app.position = data.timestamp;
+    
+    app.updatePlaylist();
+});
 
 eventSource.addEventListener('playback', e => {
-    const app = AppSingleton.get();
-    app.player.state = JSON.parse(e.data);
-    app.updatePlayBtn(app.isPlaying());
+    setPlayback(AppSingleton.get(), JSON.parse(e.data));
 });
 
 eventSource.addEventListener('playlist-jump', e => {
     const app = AppSingleton.get();
     const jump = JSON.parse(e.data);
 
-    app.player.state = jump.playback;
-    app.updatePlayBtn(app.isPlaying());
-
+    setPlayback(app, jump.playback);
     app.player.playlist.current = jump.position;
+    app.position = jump.timestamp;
 
     app.updateCurrent(app.getPlaying());
 });
 
-eventSource.addEventListener('seek', e => { });
+eventSource.addEventListener('seek', e => {
+    const app = AppSingleton.get();
+    app.position = JSON.parse(e.data);
+});
 
 eventSource.addEventListener('quitting', _ => console.log('Quitting...'));
 
@@ -57,6 +66,25 @@ eventSource.addEventListener('set-mute', e => {
 
 eventSource.addEventListener('pop-playlist', e => { });
 
-eventSource.addEventListener('pop-set-playlist', e => { });
+eventSource.addEventListener('pop-set-playlist', e => {
+    const app = AppSingleton.get();
+    const data = JSON.parse(e.data);
 
-eventSource.addEventListener('set-playlist-add-policy', e => { });
+    app.player.playlist = app.player.playlist_stack.shift();
+    app.player.playlist.current = data.position;
+
+    app.updatePlaylist();
+
+    // TODO: timestamp
+});
+
+eventSource.addEventListener('set-playlist-add-policy', e => {
+    const app = AppSingleton.get();
+    app.player.playlist.add_policy = JSON.parse(e.data);
+});
+
+function setPlayback(app, playback) {
+    app.player.state = playback;
+    app.handleStateChange();
+    app.updatePlayBtn(app.isPlaying());
+}
