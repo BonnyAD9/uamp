159142588f44d2b155af0460dd2f352222e47200
Martan03 <martinslezak03@gmail.com>
2025-10-05 20:40:23 +0200

    Add option to open artist from now playing page

--- uamp-client/ui/bar.js
+++ uamp-client/ui/bar.js
@@ -18,34 +18,40 @@ export function updatePlayBtn(playing) {
     }
 }
 
-const songIcon = document.querySelector('.bar .info .info-pic img');
-const songTitle = document.querySelector('.bar .info .title h3');
-const songArtist = document.querySelector('.bar .info .title h4');
-const barBackdrop = document.querySelector('.bar .backdrop');
+const songIcon = document.querySelector(".bar .info .info-pic img");
+const songTitle = document.querySelector(".bar .info .title h3");
+const songTitleExp = document.querySelector(".bar .info-title h3");
+const songArtist = document.querySelector(".bar .info .title h4");
+const songArtistExp = document.querySelector(".bar .info-title h4");
+const barBackdrop = document.querySelector(".bar .backdrop");
 /**
  * Updates the currently playing song info
  * @param {?Song} song - currently playing song
  */
 export function updateCurrent(song) {
     if (song === null) {
-        songTitle.textContent = 'Not Playing...';
-        songArtist.textContent = '';
+        songTitle.textContent = "Not Playing...";
+        songTitleExp.textContent = "Not Playing...";
+        songArtist.textContent = "";
+        songArtistExp.textContent = "";
         return;
     }
 
     songIcon.src = Album.getCover(song.artist, song.album);
     barBackdrop.src = Album.getCover(song.artist, song.album, 64);
     songTitle.textContent = song.title;
+    songTitleExp.textContent = song.title;
     songArtist.textContent = song.artist;
+    songArtistExp.textContent = song.artist;
 }
 
-const volumeSlider = document.getElementById('volumeSlider');
-volumeSlider.addEventListener('input', () => {
+const volumeSlider = document.getElementById("volumeSlider");
+volumeSlider.addEventListener("input", () => {
     apiCtrl(`v=${volumeSlider.value / 100}`);
 });
 
-const volumeValue = document.getElementById('volumeValue');
-const volumeIcon = document.querySelector('.volume img');
+const volumeValue = document.getElementById("volumeValue");
+const volumeIcon = document.querySelector(".volume img");
 /**
  * Updates the volume UI elements
  * @param {number} volume - current playback volume
@@ -57,14 +63,14 @@ export function updateVolume(volume, mute = false) {
     volumeValue.textContent = perVolume;
 
     const level = volume === 1.0 ? 4 : Math.ceil(volume * 3);
-    let icon = `${mute ? 'no_' : ''}volume_${level}.svg`;
+    let icon = `${mute ? "no_" : ""}volume_${level}.svg`;
     volumeIcon.src = `assets/svg/${icon}`;
 }
 
-const bar = document.querySelector('section.bar');
+const bar = document.querySelector("section.bar");
 export function toggleBar() {
-    bar.classList.toggle('expanded');
-    if (bar.classList.contains('expanded'))
+    bar.classList.toggle("expanded");
+    if (bar.classList.contains("expanded"))
         setTimeout(() => AppSingleton.get().createBarSongs(), 200);
 }
 window.toggleBar = toggleBar;
